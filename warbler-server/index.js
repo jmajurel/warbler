4657beb8a9b15e8cab5d78f7aa0f1a8d627fc3b5
j.majurel <jeanfelix.majurel@gmail.com>
2018-09-03 18:04:55 +0100

    initial'

---
+++ warbler-server/index.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorHandler = require('./handlers/error');
+const authRoutes = require('./routes/auth');
+const msgRoutes = require('./routes/messages');
+const { loginRequired, ensureCorrectUser } = require('./middleware/auth');
+
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+//all my routes here
+app.use('/api/auth', authRoutes);
+
+app.use(
+    '/api/users/:id/messages', 
+    loginRequired,
+    ensureCorrectUser,
+    msgRoutes);
+
+app.get('/api/messages', loginRequired, async function(req, res, next){
+  try {
+    let messages = await db.Message.find()
+                           .sort({createdAt: 'desc'})
+			   .populate('user', {
+			     username: true,
+			     profileImageUrl: true
+			   });
+    return res.status(200).json(messages);
+  } catch(err) {
+    return next(err);
+  }
+});
+
+//error handler
+app.use((req, res, next) => {
+  let err = new Error('NOT FOUND');
+  err.status = 404;
+  next(err);
+});
+
+app.use(errorHandler);
+
+//server listening on port
+app.listen(PORT, () => {
+ console.log(`server listening on port ${PORT}`);
+});
